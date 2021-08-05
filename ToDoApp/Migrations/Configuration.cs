7472c72fe1e6cc72c9dcd67b91202c3da8732726
mehme <mehme@LAPTOP-NES80IEJ>
2021-08-05 13:52:28 +0300

    Add project files.

---
+++ ToDoApp/Migrations/Configuration.cs
@@ -0,0 +1,35 @@
+namespace ToDoApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using ToDoApp.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ToDoApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ToDoApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+
+            AddUsers(context); //My seed method !!
+        }
+        void AddUsers(ToDoApp.Models.ApplicationDbContext context)
+        {
+            var user = new ApplicationUser { UserName = "user1@gmail.com" };
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            um.Create(user, "password");
+        }
+    }
+}
